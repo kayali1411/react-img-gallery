76c54b0005f20eda07e604c661bf4ca1c2869a9f
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-08-28 14:11:29 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    externals: {
+        'react': 'commonjs react'
+    }
+};
