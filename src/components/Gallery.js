4e9c0814408bcdd921a0a4ddf57791c6869c633c
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-09-04 22:49:44 +0300

    handle start and end thumbnail image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,12 +2,28 @@ import React, { useState, useEffect } from 'react';
 import styles from '../styles/Gallery.module.css';
 
 const Gallery = (props) => {
+    const lastThumbnail = props.children.length - 5;
     const [imgSrc, setImgSrc] = useState('');
+    const [thumbnailLeft, setThumbnailLeft] = useState(0);
     const [thumbnailHeight, setThumbnailHeight] = useState('');
+    const [currentThumbnail, setCurrentThumbnail] = useState(0);
+
+
 
     const displayImage = (e) => {
         setImgSrc(e.target.src);
     }
+
+    const handelThumbnailSlider = (e) => {
+        const value     = Number(e.target.getAttribute('data-value'));
+        const direction = value > 0 ? 'left' : 'right';
+
+        if(currentThumbnail === lastThumbnail && direction === 'right') return;
+        if(currentThumbnail === 0 && direction === 'left') return;
+
+        setCurrentThumbnail(currentThumbnail - value);
+        setThumbnailLeft(Number(thumbnailLeft) + (value * 20));
+    };
 
     useEffect(() => {
         const el = document.querySelector('#images-list img');
@@ -21,20 +37,22 @@ const Gallery = (props) => {
                 <img className={styles.slider_image} src={imgSrc} alt="" />
             </div>
             <div id="images-list" className={styles.images_list} style={{height: thumbnailHeight}}>
-                <div id="grid-slider" className={styles.grid_slider} style={{height: thumbnailHeight}}>
+                <div id="thumbnail-slider" className={styles.grid_slider} style={{height: thumbnailHeight}}>
                     {React.Children.map(props.children, (child, i) => (
                         <div
                             key={i}
                             onClick={displayImage}
                             className={styles.image_block}
                             style={{
-                                left: (i+1)*20 + '%'
+                                left: Number(((i+1)*20)+ thumbnailLeft) + '%'
                             }}
                         >
                             {child}
                         </div>
                     ))}
                 </div>
+                <button onClick={handelThumbnailSlider} data-value={1}>Left</button>
+                <button onClick={handelThumbnailSlider} data-value={-1}>Right</button>
             </div>
         </div>
     )
