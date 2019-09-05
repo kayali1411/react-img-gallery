7b4a6d99920a842646dd70aef5e38f0cfc6f9a97
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-09-05 12:06:15 +0300

    working on Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import styles from '../styles/Modal.module.css';
+import fullScreen from '../../icons/full_screen.png';
+
+const Modal = (props) => {
+    const [modalState, setModalState] = useState('closed');
+
+    const handelOpenModal = () => {
+        setModalState('open');
+    }
+
+    const handelCloseModal = () => {
+        setModalState('closed');
+    }
+
+    return (
+        <div className={styles.modal_area}>
+            <img
+                src={props.imgSrc}
+                alt={props.imgAlt}
+                className={styles.img}
+                width={props.width}
+                height={props.height}
+                onClick={handelOpenModal}
+            />
+
+            <div className={styles.modal} onClick={handelCloseModal} data-status={modalState}>
+                <span className={styles.close} onClick={handelCloseModal}>&times;</span>
+                <img className={styles.modal_image} src={props.imgSrc} alt={props.imgAlt} />
+            </div>
+        </div>
+    )
+};
+
+Modal.defaultProps = {
+    width: '100%',
+    height: '100%',
+    fullScreenIcon: false
+}
+
+export default Modal;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Gallery from './components/Gallery';
+import Modal from './components/Modal';
 
 export {
     Gallery,
+    Modal
 }
